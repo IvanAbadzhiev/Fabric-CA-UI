52bef64d2a94db6e9cd97134dd013e54a0529c93
Ivan Abadzhiev <abadzhievltd@gmail.com>
2019-01-22 09:21:58 +0200

    added enroll admin

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,22 +1,44 @@
 <template>
-  <div class="container-fluid">
-    <app-header></app-header>
-  </div>
+    <div class="container-fluid">
+        <app-header></app-header>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <fieldset>
+
+                    <legend class="w-auto">Enroll Admin identity</legend>
+
+                    <form>
+                        <div class="form-group">
+                            <label>ID Name</label>
+                            <input type="email" class="form-control" placeholder="Enter name of the identity">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" placeholder="Password">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Enroll Admin</button>
+                    </form>
+                </fieldset>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Header from './Header';
+    import Header from './Header';
 
-export default {
-  name: 'Dashboard',
-  data () {
-    return {
+    export default {
+        name: 'Dashboard',
+        data() {
+            return {}
+        },
+        components: {
+            appHeader: Header
+        }
     }
-  },
-  components : {
-    appHeader : Header
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
